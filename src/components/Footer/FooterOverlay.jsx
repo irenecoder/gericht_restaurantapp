bc012d06d18df71b4264ac7fc9fcbd5b59e8969b
irenecoder <wairene60@gmail.com>
2022-08-04 17:36:05 +0300

    findus & footer

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div>
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
     FooterOverlay
   </div>
 );
